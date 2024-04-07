10502a1a14dd06ff0ad8dc936a57639447ec5796
Rego Borsodi <regobpx@gmail.com>
2024-04-07 19:21:40 +0200

    Clean up

--- start_jupyter.ps1
+++
@@ -1,2 +0,0 @@
-conda activate global
-Start-Process python -WindowStyle hidden -ArgumentList "-m", "jupyterlab", "--no-browser", "--expose-app-in-browser", "--ServerApp.port=8888", "--ServerApp.root_dir=C:/Users/ebogrer"
